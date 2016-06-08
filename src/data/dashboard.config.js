3de0c18370f51c9bb99ed2c2bca15e5c0cea07e2
Baxter <332395368@qq.com>
2016-06-08 14:38:48 +0800

    修改配置

--- src/data/dashboard.config.js
+++ src/data/dashboard.config.js
@@ -2,11 +2,11 @@
  * Created by Baxter on 2016/5/31.
  */
 
-const DEBUG = false;
+const DEBUG = true;
 const SUPPLIER = 'npn';
 
 const hermes = {
-  host: DEBUG ? '172.16.10.20' : '172.16.10.30',
+  host: DEBUG ? '172.16.10.20' : '172.16.10.60',
   port: '8080'
 };
 
